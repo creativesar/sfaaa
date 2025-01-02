e165801fa79cd1ae9fb399a363ac53f45a561a53
creativesar <uniqueluck68@gmail.com>
2025-01-02 08:54:26 +0000

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'cdn.sanity.io'
-            }
-        ],
-        domains: ['cdn.sanity.io', 'pagedone.io'], // Add the hostnames here
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pagedone.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: "l3pqtr0z",
-  dataset: "production",
-  apiVersion: "2025-01-01",
+  projectId,
+  dataset,
+  apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
